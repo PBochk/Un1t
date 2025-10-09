e24bb978ea93157d72af2d220f071d78b689fd84
PBochk <pbochkarev06@gmail.com>
2025-10-09 17:04:18 +0500

    fix: player view flip
    Player flip now changes rotation instead of scale

--- Un1t (second iteration)/Assets/Scripts/Player/PlayerView.cs
+++ Un1t (second iteration)/Assets/Scripts/Player/PlayerView.cs
@@ -30,12 +30,13 @@ public class PlayerView : MonoBehaviour
             || moveDirection.x > 0 && !isFacingRight)
         {
             isFacingRight = !isFacingRight;
-            playerTransform.localScale = new Vector3(playerTransform.localScale.x * (-1), 
-                                                     playerTransform.localScale.y, 
-                                                     playerTransform.localScale.z);
+            //playerTransform.localScale = new Vector3(playerTransform.localScale.x * (-1), 
+            //                                         playerTransform.localScale.y, 
+            //                                         playerTransform.localScale.z);
 
             // Line above changes player's facing direction more correctly, but breaks camera
-            // playerTransform.RotateAround(playerTransform.position, Vector2.up, 180);
+            // It somehow fixed now?..
+            playerTransform.RotateAround(playerTransform.position, Vector2.up, 180);
         }
         animator.SetBool("IsRunningForward", moveDirection.x != 0);
     }
