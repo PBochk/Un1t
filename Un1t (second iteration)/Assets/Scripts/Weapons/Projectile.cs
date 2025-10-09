8445c167d335677ca65ea34a6a2f3209c37fff8e
PBochk <pbochkarev06@gmail.com>
2025-10-09 18:05:02 +0500

    feat: projectile script added

---
+++ Un1t (second iteration)/Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private float damage;
+    private float lifetime;
+    private Collider2D projectileCollider;
+    private ContactFilter2D contactFilter;
+
+    private void Awake()
+    {
+        projectileCollider = GetComponent<Collider2D>();
+    }
+
+    public void Initialize(float damage, float lifetime, LayerMask solid)
+    {
+        this.damage = damage;
+        this.lifetime = lifetime;
+        contactFilter.SetLayerMask(solid);
+        StartCoroutine(DestroyProjectile());
+    }
+
+    private IEnumerator DestroyProjectile()
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var targets = new List<Collider2D>();
+        Physics2D.OverlapCollider(projectileCollider, contactFilter, targets);
+        if (targets.Count == 0) return;
+        foreach (var target in targets)
+        {
+            var targetHealth = target.GetComponent<HealthComponent>();
+            if (targetHealth != null)
+            {
+                targetHealth.TakeDamage(damage);
+            }
+        }
+        Destroy(gameObject);
+    }
+}
