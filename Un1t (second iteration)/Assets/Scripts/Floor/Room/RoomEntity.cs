f51492cde8f307051c9cf37cd320124ad406f494
v_yashchik <medyantsevsvyt@gmail.com>
2025-10-25 23:24:43 +0500

    fix: SampleScene fixed

--- Un1t (second iteration)/Assets/Scripts/Floor/Room/RoomEntity.cs
+++ Un1t (second iteration)/Assets/Scripts/Floor/Room/RoomEntity.cs
@@ -4,26 +4,17 @@ using UnityEngine;
 /// Represents one object in a room
 /// Contains the object and its position in the room
 /// </summary>
-public abstract class RoomEntity : MonoBehaviour  
+public class RoomEntity
 {
     public GameObject GameObject { get; }
 
     public Vector3 StartPosition { get; }
 
-    public abstract void Create();
 
     public RoomEntity(GameObject gameObject, Vector3 startPosition)
     {
         GameObject = gameObject;
         StartPosition = startPosition;
-
-    }
-}
-
-public class EnemyEntity : RoomEntity
-{
-    protected override void Create()
-    {
 
     }
 }
