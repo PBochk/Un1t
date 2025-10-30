7e8123031f3521f0a77da2ff36dd288de1fe0cda
PBochk <pbochkarev06@gmail.com>
2025-10-30 18:09:40 +0500

    rework: player xp view
    Player experience view is separate UI class now (not player's component)

--- Un1t (second iteration)/Assets/Scripts/Player/PlayerUpgradeUI.cs
+++ Un1t (second iteration)/Assets/Scripts/Player/PlayerUpgradeUI.cs
@@ -1,31 +1,33 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 //TODO: make UI class instead
-public class PlayerExperienceView : MonoBehaviour
+public class PlayerUpgradeUI : MonoBehaviour
 {
     [SerializeField] private Canvas canvas;
     [SerializeField] private Button health;
     [SerializeField] private Button attackSpeed;
     [SerializeField] private Button damage;
+    [SerializeField] private PlayerModelMB playerModelMB;
     [SerializeField] private PlayerMeleeWeaponModelMB meleeModelMB;
+    //TODO: remove controller from here
+    [SerializeField] private PlayerController controller; // Shouldn't be here
+    private PlayerModel playerModel;
     private PlayerMeleeWeaponModel meleeModel;
-    private PlayerModel model;
-    private PlayerController controller;
+
     private void Awake()
     {
         health.onClick.AddListener(UpgradeHealth);
         attackSpeed.onClick.AddListener(UpgradeAttackSpeed);
         damage.onClick.AddListener(UpgradeDamage);
-        controller = GetComponent<PlayerController>();
     }
 
     private void Start()
     {
-        model = GetComponent<PlayerModelMB>().PlayerModel;
-        model.NextLevel += OnLevelUp;
+        playerModel = playerModelMB.PlayerModel;
+        playerModel.NextLevel += OnLevelUp;
         meleeModel = (PlayerMeleeWeaponModel)meleeModelMB.MeleeWeaponModel;
+        DeactivateCanvas();
     }
 
     // Experience model intialize later than OnEnable, so can't make it work rn
@@ -35,26 +37,26 @@ public class PlayerExperienceView : MonoBehaviour
     //    model.NextLevel += OnLevelUp;
     //}
 
-    private void OnDisable()
-    {
-        model.NextLevel -= OnLevelUp;
-    }
+    //private void OnDisable()
+    //{
+    //    playerModel.NextLevel -= OnLevelUp;
+    //}
 
     private void OnLevelUp()
     {
         Debug.Log("OnLevelUp");
         canvas.gameObject.SetActive(true);
-        controller.SetInputEnabled(false);
+        controller.SetInputEnabled(false); // Shouldn't be here
     }
 
     private void UpgradeHealth()
     {
-        model.UpgradeHealth();
+        playerModel.UpgradeHealth();
         DeactivateCanvas();
     }
     private void UpgradeAttackSpeed()
     { 
-        meleeModel.UpgradeAttackSpeed(model.Level);
+        meleeModel.UpgradeAttackSpeed(playerModel.Level);
         DeactivateCanvas();
     }
     private void UpgradeDamage()
@@ -66,7 +68,6 @@ public class PlayerExperienceView : MonoBehaviour
     private void DeactivateCanvas()
     {
         canvas.gameObject.SetActive(false);
-        controller.SetInputEnabled(true);
+        controller.SetInputEnabled(true); // Shouldn't be here
     }
-
 }
