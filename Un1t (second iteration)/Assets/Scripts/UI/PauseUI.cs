87c0efd99866eee8afe98c93c0f828792fe04e1b
PBochk <pbochkarev06@gmail.com>
2025-11-01 03:11:19 +0500

    rework: pause manager
    Scene managment is moved to separate class

--- Un1t (second iteration)/Assets/Scripts/UI/PauseUI.cs
+++ Un1t (second iteration)/Assets/Scripts/UI/PauseUI.cs
@@ -4,61 +4,42 @@ using UnityEngine.UI;
 
 public class PauseUI : MonoBehaviour
 {
-    [SerializeField] private PlayerModelMB playerModelMB;
+    [SerializeField] private PauseManager pauseManager;
     [SerializeField] private Canvas canvas;
     [SerializeField] private Button unpause;
     [SerializeField] private Button reload;
     [SerializeField] private Button quit;
-    private PlayerModel playerModel;
 
     private void Awake()
     {
         unpause.onClick.AddListener(UnpauseScene);
         reload.onClick.AddListener(ReloadScene);
-        quit.onClick.AddListener(QuitGame);
-        canvas.enabled = false;
+        quit.onClick.AddListener(pauseManager.QuitGame);
     }
 
     private void Start()
     {
-        playerModel = playerModelMB.PlayerModel;
+        canvas.enabled = false;
     }
-
-    //private void OnEnable()
-    //{
-    //    playerModel.PlayerDeath += OnPlayerDeath;
-    //}
 
     private void OnPause()
     {
-        canvas.enabled = true;
-        PauseScene();
-    }
-
-
-    // Below lies temporary solution for demonstration
-    // TODO: remove from here
-    private void PauseScene()
-    {
-        Time.timeScale = 0f;
-        playerModel.SetPlayerRestrained(true);
+        if (!pauseManager.IsPaused)
+        {
+            canvas.enabled = true;
+            pauseManager.PauseScene();
+        }
     }
 
     private void UnpauseScene()
     {
-        Time.timeScale = 1f;
-        playerModel.SetPlayerRestrained(false);
+        pauseManager.UnpauseScene();
         canvas.enabled = false;
     }
 
     private void ReloadScene()
     {
-        UnpauseScene();
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    private void QuitGame()
-    {
-        Application.Quit();
+        pauseManager.ReloadScene();
+        canvas.enabled = false;
     }
 }
