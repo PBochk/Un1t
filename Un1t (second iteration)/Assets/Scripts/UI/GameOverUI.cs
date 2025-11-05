e54d7b0f841c187d3568c0f2f83fad52c959670d
PBochk <pbochkarev06@gmail.com>
2025-11-05 19:54:48 +0500

    feat: game over UI win text
    Game over UI is completed with win text

--- Un1t (second iteration)/Assets/Scripts/UI/GameOverUI.cs
+++ Un1t (second iteration)/Assets/Scripts/UI/GameOverUI.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,17 +8,28 @@ using UnityEngine.UI;
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private PlayerModelMB playerModelMB;
+    //[SerializeField] private EventParent parent; // <---- serialize object that has event LevelEnded
     [SerializeField] private PauseManager pauseManager;
     [SerializeField] private Canvas canvas;
+    [SerializeField] private TMP_Text gameoverText;
+    [SerializeField] private string winText;
+    [SerializeField] private string loseText;
     [SerializeField] private Button reload;
+    [SerializeField] private Button quit;
     private PlayerModel playerModel;
+
+    private void Awake()
+    {
+        reload.onClick.AddListener(ReloadScene);
+        quit.onClick.AddListener(QuitGame);
+    }
 
     private void Start()
     {
         playerModel = playerModelMB.PlayerModel;
         // TODO: move subscription in OnEnable after model initialization rework
         playerModel.PlayerDeath += OnPlayerDeath;
-        reload.onClick.AddListener(ReloadScene);
+        //parent.LevelEnded.AddListener(OnLevelEnd); // <---- subscribe method on event
         canvas.enabled = false;
     }
 
@@ -33,10 +46,22 @@ public class GameOverUI : MonoBehaviour
     private void OnPlayerDeath()
     {
         canvas.enabled = true;
+        gameoverText.text = loseText;
+    }
+
+    private void OnLevelEnd()
+    {
+        canvas.enabled = true;
+        gameoverText.text = winText;
     }
 
     private void ReloadScene()
     {
         pauseManager.ReloadScene();
+    }
+
+    private void QuitGame()
+    {
+        pauseManager.QuitGame();
     }
 }
