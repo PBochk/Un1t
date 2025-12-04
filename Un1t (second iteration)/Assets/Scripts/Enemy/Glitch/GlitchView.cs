d21649379fefcd510d6eac97287534660ef4be72
LacoPT <izatullinkirill@gmail.com>
2025-12-04 21:09:41 +0500

    feat: glitch death animation

--- Un1t (second iteration)/Assets/Scripts/Enemy/Glitch/GlitchView.cs
+++ Un1t (second iteration)/Assets/Scripts/Enemy/Glitch/GlitchView.cs
@@ -1,7 +1,19 @@
+using UnityEngine;
+
+[RequireComponent(typeof(DeadState))]
+[RequireComponent(typeof(Animator))]
 public class GlitchView : EnemyView
 {
+    private DeadState deadState;
+    private Animator animator;
     protected override void BindStates()
     {
+        DeadState deadState = GetComponent<DeadState>();
+        Animator animator = GetComponent<Animator>();
+        deadState.OnStateEnter.AddListener(() =>
+        {
+            animator.SetTrigger("Dead");
+        });
     }
 
     protected override void BindAnimator()
