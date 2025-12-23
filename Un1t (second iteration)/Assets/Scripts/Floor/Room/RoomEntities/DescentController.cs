2be1e99d0aab6357599408e17c95c5cc68e91421
v_yashchik <medyantsevsvyt@gmail.com>
2025-12-23 14:55:46 +0500

    feat: descent enterance event added

--- Un1t (second iteration)/Assets/Scripts/Floor/Room/RoomEntities/DescentController.cs
+++ Un1t (second iteration)/Assets/Scripts/Floor/Room/RoomEntities/DescentController.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DescentController : MonoBehaviour
 {
+    public UnityEvent OnPlayerEntered;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //This functional is for demo only
-        if (!collision.TryGetComponent(out PlayerController player)) return;
-        GameObject.FindWithTag("LevelEnder").GetComponent<EventParent>().NotifyLevelEnded();
-        Debug.Log("Level completed!!!");
+        if (!collision.TryGetComponent<PlayerController>(out _)) return;
+        FindFirstObjectByType<EventParent>().NotifyLevelEnded();
 
+        OnPlayerEntered?.Invoke();
     }
 }
