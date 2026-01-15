f16d41016d884d91abfd705ca92e2c743b9be11e
PBochk <pbochkarev06@gmail.com>
2026-01-15 23:04:11 +0500

    fix: gameover UI

--- Un1t (second iteration)/Assets/Scripts/UI/GameOverUI.cs
+++ Un1t (second iteration)/Assets/Scripts/UI/GameOverUI.cs
@@ -12,14 +12,15 @@ public class GameOverUI : MonoBehaviour
     [SerializeField] private TMP_Text gameoverText;
     [SerializeField] private string winText;
     [SerializeField] private string loseText;
-    [SerializeField] private Button reload;
+    [SerializeField] private Button toMenu;
     [SerializeField] private Button quit;
 
     private void Awake()
     {
         canvas.worldCamera = Camera.current;
-        reload.onClick.AddListener(ReloadScene);
+        toMenu.onClick.AddListener(ToMenu);
         quit.onClick.AddListener(QuitGame);
+        canvas.enabled = false;
     }
 
     private void Start()
@@ -42,7 +43,7 @@ public class GameOverUI : MonoBehaviour
     public void BindEvents(UIAudio audio, PlayerModel playerModel)
     {
         playerModel.PlayerDeath += OnPlayerDeath;
-        reload.onClick.AddListener(audio.PlayButtonClickSound);
+        toMenu.onClick.AddListener(audio.PlayButtonClickSound);
         quit.onClick.AddListener(audio.PlayButtonClickSound);
     }
 
@@ -61,15 +62,15 @@ public class GameOverUI : MonoBehaviour
         gameoverText.text = loseText;
     }
 
-    private void OnLevelEnd()
+    private void OnGameCompleted()
     {
         canvas.enabled = true;
         gameoverText.text = winText;
     }
 
-    private void ReloadScene()
+    private void ToMenu()
     {
-        EntryPoint.Instance.ReloadScene();
+        EntryPoint.Instance.LoadMenu();
     }
 
     private void QuitGame()
